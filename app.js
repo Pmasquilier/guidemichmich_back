d796a291913e8a1d807a3438c5ef1c1ef5740ccf
Pierre Masquilier <pierremasquilier@gmail.com>
2022-04-01 21:16:19 +0700

    update aws config

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 
-
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const userModel = require("./models");
 
@@ -12,11 +11,13 @@ app.use(cors());
 // Load the AWS SDK for Node.js
 var AWS = require("aws-sdk");
 // Set the region
-AWS.config.update({ region: "eu-west-3" });
-
+AWS.config.update({
+  region: "eu-west-3",
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 // Create S3 service object
 s3 = new AWS.S3({ apiVersion: "2006-03-01" });
-
 
 /* mongoose.connect('mongodb+srv://guide_mich_mich_atlasdb_user:a6A3Ms7JH6mD5TRSGnhFgk8unU@cluster0.1j9ta.mongodb.net/guide_mich_mich_db?retryWrites=true&w=majority',
     {
@@ -31,50 +32,47 @@ db.once("open", function () {
     console.log("Connected successfully");
 }); */
 
-
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
+app.get("/", (req, res) => {
+  res.status(200).send("Hello server is running").end();
 });
 
-app.get('/details', async (req, res) => {/* 
+app.get("/details", async (req, res) => {
+  /* 
     res.send({data: 'Hello World, from express'}); */
-    const user = await userModel.findOne({id: 123});
-    //const user = await userModel.find({});
-    console.log('user ' + user);
-    res.send(user);
+  const user = await userModel.findOne({ id: 123 });
+  //const user = await userModel.find({});
+  console.log("user " + user);
+  res.send(user);
 });
-
 
 app.get("/images", async (req, res) => {
+  console.log(req.params.pays);
+  var bucketParams = {
+    Bucket: "guidemichmich",
+    Prefix: req.query.pays,
+  };
 
-    console.log(req.params.pays)
-    var bucketParams = {
-      Bucket: "guidemichmich",
-      Prefix: req.query.pays
-    };
-  
-    s3.listObjects(bucketParams, function (err, data) {
-      if (err) {
-        console.log("Error", err);
-      } else {
-        console.log("Success");
-        var urls = [];
-        for (let index = 1; index < data['Contents'].length; index++) {
-            url = 'https://guidemichmich.s3.eu-west-3.amazonaws.com/' + data['Contents'][index]['Key'];
-            console.log(url);
-            urls.push(url);        
-        }
-        res.send(urls);
+  s3.listObjects(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success");
+      var urls = [];
+      for (let index = 1; index < data["Contents"].length; index++) {
+        url =
+          "https://guidemichmich.s3.eu-west-3.amazonaws.com/" +
+          data["Contents"][index]["Key"];
+        console.log(url);
+        urls.push(url);
       }
-    });
+      res.send(urls);
+    }
   });
+});
 /* app.use((req, res, next) => {
   console.log("Requête reçus");
   next();
